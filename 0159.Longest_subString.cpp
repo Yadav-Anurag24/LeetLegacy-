b12ed3219ac057d382a85c03aef463f0deada8e7
Anurag Kumar <msdanurag65@gmail.com>
2025-08-05 11:16:09 +0530

    Longest Substring with distinct chars

---
+++ 0159.Longest_subString.cpp
@@ -0,0 +1,12 @@
+// Find the longest substring which has the at most two distinct characters. Return that length of the substring.
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution{
+    public:
+    int LongestSubstring(string s){
+        
+    }
+
+};
